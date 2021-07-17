5250c0a24a9272799de520bffd304aa6ba0fdbb1
GTPE Student <avishnia-redhat.com@bastion.cb04.internal>
2021-07-17 05:02:44 +0000

    Phase 1. route53

--- add-dns.sh
+++ add-dns.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 domainname=$1
-cat > gitlab-record.json <<- "EOF"
+cat > gitlab-record.json <<EOF
 {
             "Comment": "CREATE/DELETE/UPSERT a record ",
             "Changes": [{
-            "Action": "CREATE",
+            "Action": "UPSERT",
                         "ResourceRecordSet": {
                                     "Name": "gitlab.$domainname.",
                                     "Type": "NS",
                                     "TTL": 300,
                                  "ResourceRecords": [
                                    {
-                                     "Value": "ns-411.awsdns-51.com"
+                                     "Value": "ns-707.awsdns-24.net"
                                    },
                                    {
-                                     "Value": "ns-1841.awsdns-38.co.uk"
+                                     "Value": "ns-385.awsdns-48.com"
                                    },
                                    {
-                                     "Value": "ns-909.awsdns-49.net"
+                                     "Value": "ns-1217.awsdns-24.org"
                                    },
                                    {
-                                     "Value": "ns-1074.awsdns-06.org"
+                                     "Value": "ns-1754.awsdns-27.co.uk"
                                    }
                                  ]
                        }}]
 }
 EOF
 zoneid=`aws route53 list-hosted-zones-by-name |  jq --arg name "${domainname}." -r '.HostedZones | .[] | select(.Name=="\($name)") | .Id'`
-changeid=`aws route53 change-resource-record-sets --hosted-zone-id /hostedzone/$zoneid --change-batch file://gitlab-record.json |grep '"Id"' |awk -F'"' {'print $4'}`
+changeid=`aws route53 change-resource-record-sets --hosted-zone-id $zoneid --change-batch file://gitlab-record.json |grep '"Id"' |awk -F'"' {'print $4'}`
 while [ `aws route53  get-change --id $changeid |grep Status |awk -F'"' {'print $4'}` != 'INSYNC'  ]
 do
 sleep 1
