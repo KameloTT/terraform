de8756b89c3c297ed9c5c05b3a09712a3830027e
GTPE Student <avishnia-redhat.com@bastion.54e7.internal>
2021-09-17 05:01:41 +0000

    Add async dr for mysql

--- scripts/setup-pwx-async-dr.sh
+++ scripts/setup-pwx-async-dr.sh
@@ -12,12 +12,20 @@ oc1 cp -n kube-system $STORK_POD:/storkctl/linux/storkctl ./storkctl
 sudo mv storkctl /usr/local/bin &&
 sudo chmod +x /usr/local/bin/storkctl
 
-echo "Primary CLuster. Create Credentials"
-pod=`oc1 get pods -l name=portworx -n kube-system -o jsonpath='{.items[0].metadata.name}'`
-uuid=`oc1 exec $pod -n kube-system -- /opt/pwx/bin/pxctl status 2>/dev/null | grep UUID | awk '{print $3}'`
-oc1 exec $pod -n kube-system -- /opt/pwx/bin/pxctl credentials create --provider s3 --s3-access-key $key --s3-secret-key $secret --s3-region $region  --s3-endpoint s3.amazonaws.com --s3-storage-class STANDARD clusterPair_$uuid
 
 echo "Secondary CLuster. Create Credentials"
 pod=`oc2 get pods -l name=portworx -n kube-system -o jsonpath='{.items[0].metadata.name}'`
 uuid=`oc2 exec $pod -n kube-system -- /opt/pwx/bin/pxctl status 2>/dev/null | grep UUID | awk '{print $3}'`
+oc2 exec $pod -n kube-system -- /opt/pwx/bin/pxctl license activate 5c73-e11f-242a-47a7-ad50-e283-dd17-b22b
 oc2 exec $pod -n kube-system -- /opt/pwx/bin/pxctl credentials create --provider s3 --s3-access-key $key --s3-secret-key $secret --s3-region $region  --s3-endpoint s3.amazonaws.com --s3-storage-class STANDARD clusterPair_$uuid
+
+echo "Primary CLuster. Create Credentials"
+pod=`oc1 get pods -l name=portworx -n kube-system -o jsonpath='{.items[0].metadata.name}'`
+oc1 exec $pod -n kube-system -- /opt/pwx/bin/pxctl license activate 5c73-e11f-242a-47a7-ad50-e283-dd17-b22b
+oc1 exec $pod -n kube-system -- /opt/pwx/bin/pxctl credentials create --provider s3 --s3-access-key $key --s3-secret-key $secret --s3-region $region  --s3-endpoint s3.amazonaws.com --s3-storage-class STANDARD clusterPair_$uuid
+
+ansible-playbook ansible/cluster-pair-mysql.yaml
+
+oc1 apply -f ansible/files/migrationpolicy_mysql.yaml
+
+oc1 apply -f ansible/files/migration_schedule_mysql.yaml
--- scripts/setup-pwx-cluster.sh
+++ scripts/setup-pwx-cluster.sh
@@ -7,6 +7,8 @@ alias oc1='kubectl --kubeconfig=kubeconfig_primary-cluster'
 alias oc2='kubectl --kubeconfig=kubeconfig_secondary-cluster'
 
 oc1 create namespace $project --dry-run=client -o yaml | oc1 apply -f -
+oc1 create secret generic --from-file=$HOME/.aws/credentials -n $project aws-creds
+
 oc1 -n $project create secret generic alertmanager-portworx --from-file=ansible/files/alertmanager.yaml --dry-run=client -o yaml | oc1 apply -f -
 oc1 -n $project apply -f https://install.portworx.com/2.8?comp=pxoperator
 sleep 5
@@ -17,6 +19,12 @@ done
 
 oc1 -n $project apply -f "https://install.portworx.com/2.8?operator=true&mc=false&kbver=1.20.7-eks-d88609&b=true&kd=type%3Dgp2%2Csize%3D150&csicd=true&s=%22type%3Dgp2%2Csize%3D150%22&j=auto&c=px-primary-e08f7201-b224-4a5f-b9a5-b61cba23297f&eks=true&stork=true&csi=true&mon=true&st=k8s&e=AWS_ACCESS_KEY_ID%3D${key}%2CAWS_SECRET_ACCESS_KEY%3D${secret}&promop=true"
 oc1 apply -f ansible/files/pwx-svc-ext.yaml
+
+while [ `oc1 get storageclusters.core.libopenstorage.org/px-primary-e08f7201-b224-4a5f-b9a5-b61cba23297f -n kube-system 2>/dev/null -o name`1 != 'storagecluster.core.libopenstorage.org/px-primary-e08f7201-b224-4a5f-b9a5-b61cba23297f'1 ]
+do 
+  echo "Waiting StorageCluster Object"
+done
+oc1 patch -n kube-system --type merge storageclusters.core.libopenstorage.org/px-primary-e08f7201-b224-4a5f-b9a5-b61cba23297f -p '{"spec":{"stork":{"volumes":[{"mountPath":"/root/.aws/","name":"aws-creds","secret":{"secretName":"aws-creds"}}]}}}'
 
 oc2 create namespace $project --dry-run=client -o yaml | oc2 apply -f -
 oc2 -n $project create secret generic alertmanager-portworx --from-file=ansible/files/alertmanager.yaml --dry-run=client -o yaml | oc2 apply -f -
